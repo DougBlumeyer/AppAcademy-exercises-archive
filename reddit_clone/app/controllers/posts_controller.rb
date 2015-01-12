566a1883250ae9147a21caf219c7aeda7c4b3127
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2015-01-11 18:09:35 -0800

    phase II complete

--- reddit_clone/app/controllers/posts_controller.rb
+++ reddit_clone/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :only_correct_user!, only: [:edit, :update]
 
   def new
     @post = Post.new(sub_id: params[:sub_id].to_i)
@@ -18,9 +19,27 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :url, :content, :sub_id)
+  end
+
+  def only_correct_user!
+    redirect_to user_url(current_user) if current_user != Post.find(params[:id]).author
   end
 end
--- reddit_clone/config/routes.rb
+++ reddit_clone/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show] do
-    resources :subs, only: [:new]
+    resources :subs, only: [:create]
   end
   resource :session, only: [:new, :create, :destroy]
 
-  resources :subs, except: [:new] do
-    resources :posts, only: [:new]
+  resources :subs, except: [:create] do
+    resources :posts, only: [:create]
   end
 
-  resources :posts, except: [:new, :index, :destroy]
+  resources :posts, except: [:create, :index, :destroy]
 
   root to: "sessions#new"
 end
