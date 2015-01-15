9693cfa937708f4dac49c0aef7337ef862d4ba1f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2015-01-14 16:27:48 -0800

    collapsed into normal file structure

--- active_record_lite/lib/04_associatable2.rb
+++ active_record_lite/lib/04_associatable2.rb
@@ -9,28 +9,7 @@ module Associatable
       through_options = self.class.assoc_options[through_name]
       source_options = through_options.model_class.assoc_options[source_name]
 
-      p through_options
-      p through_options.table_name
-      p through_options.model_class
-      p source_options
-      p source_options.table_name
-      p source_options.model_class
-
       results = DBConnection.execute(<<-SQL)
-        -- SELECT
-        --   *
-        -- FROM
-        --   #{source_options.table_name}
-        -- JOIN
-        --   #{through_options.table_name}
-        -- ON #{source_options.table_name}.#{source_options.primary_key} =
-        --    #{through_options.table_name}.#{through_options.foreign_key}
-        -- WHERE
-        -- --#{source_options.table_name}.#{source_options.primary_key} =
-        -- #{through_options.table_name}.#{through_options.foreign_key} =
-        --   #{send(through_options.foreign_key)}
-
-
         SELECT
           #{source_options.table_name}.*
         FROM
@@ -38,11 +17,11 @@ module Associatable
         JOIN
           #{through_options.table_name}
         ON
-        #{through_options.table_name}.#{source_options.foreign_key} =
-        #{source_options.table_name}.#{through_options.primary_key}
+          #{through_options.table_name}.#{source_options.foreign_key} =
+          #{source_options.table_name}.#{through_options.primary_key}
         WHERE
-        #{through_options.table_name}.#{through_options.primary_key} =
-        #{send(through_options.primary_key)}
+          #{through_options.table_name}.#{through_options.primary_key} =
+          #{send(through_options.foreign_key)}
       SQL
 
       source_options.model_class.parse_all(results).first
